7313f08d7d57d7a64aeb0d90cb8931e4b0ac722d
RAJAT VIJAY <rajatvijay5@gmail.com>
2020-06-04 10:13:42 +0530

    array-utils

--- js-exercises/array-utils/arrayUtils.js
+++ js-exercises/array-utils/arrayUtils.js
@@ -2,7 +2,7 @@ const isObject = (item) => item !== undefined && item !== null;
 const getLength = (item) => item.length;
 const isCallable = (fn) => typeof fn === 'function';
 
-function forEach(item, callback, thisArg) {
+function reduce(item, callback, initialValue) {
   if (!isObject(item)) {
     throw new Error(`TypeError: can't conver ${item} to object`);
   }
@@ -11,64 +11,89 @@ function forEach(item, callback, thisArg) {
   }
   const length = getLength(item);
 
-  // eslint-disable-next-line
-  for (let i = 0; i < length; i++) {
-    callback.call(thisArg, item[String(i)], i, item);
+  let accumulator;
+  let hasValue = false;
+  let i = 0;
+  if (initialValue === undefined) {
+    // eslint-disable-next-line
+    for (; i < length; i++) {
+      if (hasValue) {
+        break;
+      }
+      if (String(i) in item) {
+        accumulator = item[String(i)];
+        hasValue = true;
+      }
+    }
+
+    if (!hasValue) {
+      throw new Error('TypeError: reduce of empty array with no initial value');
+    }
+  } else {
+    accumulator = initialValue;
   }
+
+  // eslint-disable-next-line
+  for (; i < length; i++) {
+    accumulator = callback(accumulator, item[String(i)], i, item);
+  }
+
+  return accumulator;
+}
+
+function forEach(item, callback, thisArg) {
+  // if (!isObject(item)) {
+  //   throw new Error(`TypeError: can't conver ${item} to object`);
+  // }
+  // if (!isCallable(callback)) {
+  //   throw new Error(`TypeError: ${callback} is not a function`);
+  // }
+  // const length = getLength(item);
+
+  // // eslint-disable-next-line
+  // for (let i = 0; i < length; i++) {
+  //   callback.call(thisArg, item[String(i)], i, item);
+  // }
+
+  // Not sure if this is the right way
+  // since it returns something and we are plainly ignoring it
+  // But this helps in reusing the validation and looping logic
+  // from reduce method
+  reduce(
+    item,
+    (acc, ...restArgs) => {
+      callback.call(thisArg, ...restArgs);
+      return acc;
+    },
+    null,
+  );
 }
 
 function map(item, callback, thisArg) {
-  if (!isObject(item)) {
-    throw new Error(`TypeError: can't conver ${item} to object`);
-  }
-  if (!isCallable(callback)) {
-    throw new Error(`TypeError: ${callback} is not a function`);
-  }
-  const length = getLength(item);
-
-  const result = [];
-  // eslint-disable-next-line
-  for (let i = 0; i < length; i++) {
-    result.push(callback.call(thisArg, item[String(i)], i, item));
-  }
-
-  return result;
+  return reduce(
+    item,
+    (acc, ...restArgs) => {
+      acc.push(callback.call(thisArg, ...restArgs));
+      return acc;
+    },
+    [],
+  );
 }
 
 function filter(item, callback, thisArg) {
-  if (!isObject(item)) {
-    throw new Error(`TypeError: can't conver ${item} to object`);
-  }
-  if (!isCallable(callback)) {
-    throw new Error(`TypeError: ${callback} is not a function`);
-  }
-  const length = getLength(item);
-  const result = [];
-  // eslint-disable-next-line
-  for (let i = 0; i < length; i++) {
-    const output = callback.call(thisArg, item[String(i)], i, item);
-    if (output) {
-      result.push(item[String(i)]);
-    }
-  }
-
-  return result;
+  return reduce(
+    item,
+    (acc, value, ...restArgs) => {
+      const output = callback.call(thisArg, value, ...restArgs);
+      if (output) {
+        acc.push(value);
+      }
+      return acc;
+    },
+    [],
+  );
 }
 
-// function reduce(item, callback, initialValue) {
-//   if (!isObject(item)) {
-//     throw new Error(`TypeError: can't conver ${item} to object`);
-//   }
-//   if (!isCallable(callback)) {
-//     throw new Error(`TypeError: ${callback} is not a function`);
-//   }
-//   const length = getLength(item);
-//   let previousValue = initialValue;
-//   for (let i = 0; i < length; i++) {
-//     previousValue = callback(previousValue, item[String(i)], i, item);
-//   }
-
-//   return previousValue;
-// }
-
-export { forEach, map, filter };
+export {
+  forEach, map, filter, reduce,
+};
--- js-exercises/array-utils/arrayUtils.test.js
+++ js-exercises/array-utils/arrayUtils.test.js
@@ -1,4 +1,4 @@
-import { forEach, map, filter } from "./arrayUtils";
+import { forEach, map, filter, reduce } from "./arrayUtils";
 
 describe("forEach", () => {
   it("should iterate on an array", () => {
@@ -25,7 +25,7 @@ describe("forEach", () => {
   });
 
   it("should throw when args are improper", () => {
-    expect(() => forEach(null, fn)).toThrow();
+    expect(() => forEach(null, () => {})).toThrow();
     expect(() => forEach([1], null)).toThrow();
   });
 });
@@ -55,8 +55,8 @@ describe("map", () => {
   });
 
   it("should throw when args are improper", () => {
-    expect(() => forEach(null, fn)).toThrow();
-    expect(() => forEach([1], null)).toThrow();
+    expect(() => map(null, () => {})).toThrow();
+    expect(() => map([1], null)).toThrow();
   });
 });
 
@@ -85,37 +85,59 @@ describe("filter", () => {
   });
 
   it("should throw when args are improper", () => {
-    expect(() => forEach(null, fn)).toThrow();
-    expect(() => forEach([1], null)).toThrow();
+    expect(() => filter(null, () => {})).toThrow();
+    expect(() => filter([1], null)).toThrow();
   });
 });
 
-// describe("reduce", () => {
-//   it("should iterate on an array", () => {
-//     const fn = jest.fn().mockImplementation((acc, value) => acc + value);
-//     const result = reduce([1, 2, 3, 4, 5], fn, 0);
-//     expect(fn).toHaveBeenCalledWith(0, 1, 0, [1, 2, 3, 4, 5]);
-//     expect(fn).toHaveBeenCalledWith(1, 2, 1, [1, 2, 3, 4, 5]);
-//     expect(fn).toHaveBeenCalledWith(3, 3, 2, [1, 2, 3, 4, 5]);
-//     expect(fn).toHaveBeenCalledWith(6, 4, 3, [1, 2, 3, 4, 5]);
-//     expect(fn).toHaveBeenCalledWith(11, 5, 4, [1, 2, 3, 4, 5]);
-//     expect(result).toEqual([1]);
-//   });
+describe("reduce", () => {
+  it("should iterate on an array", () => {
+    const fn = jest.fn().mockImplementation((acc, value) => acc + value);
+    const result = reduce([1, 2, 3, 4, 5], fn, 0);
+    expect(fn).toHaveBeenCalledWith(0, 1, 0, [1, 2, 3, 4, 5]);
+    expect(fn).toHaveBeenCalledWith(1, 2, 1, [1, 2, 3, 4, 5]);
+    expect(fn).toHaveBeenCalledWith(3, 3, 2, [1, 2, 3, 4, 5]);
+    expect(fn).toHaveBeenCalledWith(6, 4, 3, [1, 2, 3, 4, 5]);
+    expect(fn).toHaveBeenCalledWith(10, 5, 4, [1, 2, 3, 4, 5]);
+    expect(result).toEqual(15);
+  });
 
-//   // it("should iterate an pseudo array", () => {
-//   //   const fn = jest.fn().mockImplementation((value) => value === 1);
-//   //   const item = { "0": 1, "1": 2, "2": 3, "3": 4, "4": 5, length: 5 };
-//   //   const result = filter(item, fn);
-//   //   expect(fn).toHaveBeenCalledWith(1, 0, item);
-//   //   expect(fn).toHaveBeenCalledWith(2, 1, item);
-//   //   expect(fn).toHaveBeenCalledWith(3, 2, item);
-//   //   expect(fn).toHaveBeenCalledWith(4, 3, item);
-//   //   expect(fn).toHaveBeenCalledWith(5, 4, item);
-//   //   expect(result).toEqual([1]);
-//   // });
+  it("should iterate an pseudo array", () => {
+    const fn = jest.fn().mockImplementation((acc, value) => acc + value);
+    const item = { "0": 1, "1": 2, "2": 3, "3": 4, "4": 5, length: 5 };
+    const result = reduce(item, fn, 0);
+    expect(fn).toHaveBeenCalledWith(0, 1, 0, item);
+    expect(fn).toHaveBeenCalledWith(1, 2, 1, item);
+    expect(fn).toHaveBeenCalledWith(3, 3, 2, item);
+    expect(fn).toHaveBeenCalledWith(6, 4, 3, item);
+    expect(fn).toHaveBeenCalledWith(10, 5, 4, item);
+    expect(result).toEqual(15);
+  });
 
-//   it("should throw when args are improper", () => {
-//     expect(() => forEach(null, fn)).toThrow();
-//     expect(() => forEach([1], null)).toThrow();
-//   });
-// });
+  it("should iterate on array of undefineds", () => {
+    const fn = jest
+      .fn()
+      .mockImplementation((acc, value) => String(acc) + String(value));
+    const item = [undefined, undefined];
+    const result = reduce(item, fn);
+    expect(fn).toHaveBeenCalledWith(undefined, undefined, 1, item);
+    expect(result).toEqual("undefinedundefined");
+  });
+
+  it("should ignore empty items in the array", () => {
+    const fn = jest
+      .fn()
+      .mockImplementation((acc, value) => String(acc) + String(value));
+    const item = [, , , , undefined, undefined];
+    const result = reduce(item, fn);
+    expect(fn).toHaveBeenCalledWith(undefined, undefined, 5, item);
+    expect(result).toEqual("undefinedundefined");
+  });
+
+  it("should throw when args are improper", () => {
+    expect(() => reduce(null, () => {})).toThrow();
+    expect(() => reduce([1], null)).toThrow();
+    expect(() => reduce([], () => {})).toThrow();
+    expect(() => reduce([, , , , ,], () => {})).toThrow();
+  });
+});
